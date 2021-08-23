b71e1009c78b9bf12069b5813286596c302ffc19
sagarmachine <sagarmonkey800@gmail.com>
2021-08-23 13:10:06 +0530

    staff order controller fixed

--- src/main/java/in/timesinternet/foodbooking/contoller/staff/StaffOrderController.java
+++ src/main/java/in/timesinternet/foodbooking/contoller/staff/StaffOrderController.java
@@ -6,10 +6,7 @@ import in.timesinternet.foodbooking.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -20,7 +17,7 @@ public class StaffOrderController {
     @Autowired
     OrderService orderService;
 
-    @PutMapping("/orderId/status")
+    @PatchMapping("/status")
     @PreAuthorize("hasRole('ROLE_OWNER') or hasRole('ROLE_MANAGER')")
     ResponseEntity<Order> updateOrderStatus(@RequestBody OrderStatusDto orderStatusDto, HttpServletRequest request){
         String userEmail =(String)request.getAttribute("userEmail");
