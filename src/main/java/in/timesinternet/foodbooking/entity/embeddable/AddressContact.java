d9e6fc1f393f56f3e0cfcfb43ef60f6bfea2e547
RamSirg <ram.kumar1@timesinternet.in>
2021-08-26 19:29:55 +0530

    validation on addressContact

--- src/main/java/in/timesinternet/foodbooking/entity/embeddable/AddressContact.java
+++ src/main/java/in/timesinternet/foodbooking/entity/embeddable/AddressContact.java
@@ -6,6 +6,9 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Embeddable
@@ -17,14 +20,19 @@ public class AddressContact{
     String line1;
     String line2;
     @NotNull(message = "Pincode can't be nUll")
+    @Max(value = 6,message = "pincode length is greater than 6")
+    @Min(value = 6,message = "pincode length is lesser than 6")
     Integer pincode;
     @NotNull(message = "city can't be null")
     String city;
     @NotNull(message = "state can't be null")
     String state;
+    @Email(message = "Invalid email")
     @NotNull(message = "email can't be null")
     String contactEmail;
     @NotNull(message = "contact number can't be null")
+    @Max(value = 10,message = "contact number length is greater than 10")
+    @Min(value = 10,message = "contact number length is lesser than 10")
     String contactNumber;
     @NotNull(message = "firstname can't be null")
     String contactFirstName;
